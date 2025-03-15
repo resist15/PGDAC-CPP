d3ae68aabf90c99dddf4a51023a473d5cd0a4f5e
resist15 <sourav@aospa.co>
2025-03-15 16:45:45 +0530

    PGDAC: CPP: Day11 [A1-Template]

---
+++ Day11/FuncTemplate/Main.cpp
@@ -0,0 +1,24 @@
+#include "TemplateDay11.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+	double p=10.20, k=20.10;
+	Swap(p,k);
+	cout<<"\nSwapped double variables"<<p<<" "<<k;
+	int a=10, b=20;
+	Swap(a,b);
+	cout<<"\nSwapped int variables"<<a<<" "<<b;
+	cout<<"Addition of double: "<<Add(p, k)<<endl;
+	cout<<"Addition of int: "<<Add(a, b)<<endl;
+	cout<<"Substraction of double: "<<Subtract(p, k)<<endl;
+	cout<<"Substraction of int: "<<Subtract(a, b)<<endl;
+	cout<<"Multiplication of double: "<<Multiply(p, k)<<endl;
+	cout<<"Multiplication of int: "<<Multiply(a, b)<<endl;
+	cout<<"Division of double: "<<Divide(p, k)<<endl;
+	cout<<"Division of int: "<<Divide(a, b)<<endl;
+	return 0;
+}
+
---
+++ Day11/FuncTemplate/TemplateDay11.h
@@ -0,0 +1,37 @@
+#ifndef ___TEMPLATEDAY11_H___
+#define ___TEMPLATEDAY11_H___
+
+template<class T>
+void Swap(T &x, T &y)
+{
+     T temp = x;
+     x = y; 
+     y = temp;
+}
+
+template<class T>
+T Add(T x, T y)
+{
+     return x + y;
+}
+
+template<class T>
+T Subtract(T x, T y)
+{
+     return x - y;
+}
+
+template<class T>
+T Divide(T x, T y)
+{
+     return x / y;
+}
+
+template<class T>
+T Multiply(T x, T y)
+{
+     return x * y;
+}
+
+#endif
+
